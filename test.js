80636be27e32ad94d79dfbda2af56a6d75b625d7
Igor <verycooleagle@gmail.com>
2018-01-09 19:54:29 +0200

    Remove unnecassery file.

--- test.js
+++
@@ -1,7 +0,0 @@
-const util = require('util')
-let nes2tree = require('./src/index');
-let data = require('./test/assets/data');
-
-// TODO: clear
-// console.log('res', data);
-console.log(util.inspect(nes2tree(data), {showHidden: false, depth: null}))
